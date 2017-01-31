b088011fba0f186ebad2e3a4fa9d545cc67d06f5
PatDeFruit <psegui01@gmail.com>
2017-01-31 17:40:47 +0100

    modification connexion

--- connexion.php
+++ connexion.php
@@ -54,21 +54,71 @@
 								echo '
 								<div class="row">
 									<div class="col-lg-offset-3 col-lg-6">
-										<div class="panel panel-default">
+										<div id="loginbox" class="panel panel-default">
 											<div class="panel-heading" style="background: #0C9277">
-												<h3 class="panel-title text-center">Connexion</h3>
+												<h3 class="panel-title text-center"><b>Connexion</b></h3>
+												<div style="float:right; font-size: 80%; position: relative; top:-10px;"><a style="color:white;" href="#">Forgot password?</a></div>
 											</div>
 											<div class="panel-body">
 												<form method="post" action="connexion.php">
 												<fieldset>
-												<p>
-												<label for="Mail"> Adresse mail :</label><input name="Mail" type="text" id="Mail" /><br />
-												<label for="pswd"> Mot de passe :</label><input type="password" name="pswd" id="pswd" />
-												</p>
+													<div style="margin-bottom: 25px" class="input-group">
+														<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+														<input id="Mail" type="text" class="form-control" name="Mail" value="" placeholder="eMail adress">                                        
+													</div>
+													
+													<div style="margin-bottom: 25px" class="input-group">
+														<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+														<input id="pswd" type="password" class="form-control" name="pswd" placeholder="password">
+													</div>
 												</fieldset>
-												<p><input type="submit" value="Connexion" /></p></form>
+												
+												<div class="col-sm-12 controls">
+													<input type="submit" value="Connexion" class="btn btn-success"/></p></form>
+												</div>
+												
+												<div class="form-group">
+													<div class="col-md-12 control">
+														<div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+															Don\'t have an account! 
+																<a href="#" onClick="$(\'#loginbox\').hide(); $(\'#signupbox\').show();">
+																	Sign Up Here
+																</a>
+														</div>
+													</div>
+												</div>    
 											</div>
 										</div>
+								<!-- ------------------------------------------------ -->		
+										<div id="signupbox" class="panel panel-default" style="display:none;">
+											<div class="panel-heading" style="background: #0C9277">
+												<h3 class="panel-title text-center"><b>Sign Up</b></h3>
+												<div style="float:right; font-size: 85%; position: relative; top:-10px"><a style="color:white;" id="signinlink" href="#" onclick="$(\'#signupbox\').hide(); $(\'#loginbox\').show()">Sign In</a></div>
+											</div>
+											<div class="panel-body">
+												<form method="post" action="">
+												<fieldset>
+													<div class="form-group">
+														<label for="email" class="col-md-3 control-label">Email</label>
+														<div class="col-md-9">
+															<input type="text" class="form-control" name="email" placeholder="Email Address">
+														</div>
+													</div>
+													
+													<div class="form-group">
+														<label for="password" class="col-md-3 control-label">Password</label>
+														<div class="col-md-9">
+															<input type="password" class="form-control" name="passwd" placeholder="Password">
+														</div>
+													</div>
+												</fieldset>
+												
+												<div class="col-sm-12 controls">
+													<input type="submit" value="Submit" class="btn btn-primary"/></p></form>
+												</div>
+											</div>
+										</div>
+										
 									</div>
 								</div>								 	
 								';
