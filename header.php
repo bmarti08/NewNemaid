b4f0f753c9ccf316733ee03b80f6c7202b703e80
bmarti08 <martinbenoit.79@hotmail.fr>
2017-01-31 17:43:54 +0100

    yolo

--- header.php
+++ header.php
@@ -1,4 +1,15 @@
 <!-- Navigation -->
+<style>
+	.dropdown-submenu {
+		position: relative;
+	}
+	
+	.dropdown-submenu .dropdown-menu {
+		top: 0;
+		left: 100%;
+		margin-top: -1px;
+	}
+</style>
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -13,70 +24,53 @@
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    
+                <ul class="nav navbar-nav navbar-right">                    
 					<li>
                         <a href="SelectGenus.php">SelectGenus</a>
                     </li>
                     <li>
-                        <a href="#">Sample Data Entry</a>
+                        <a href="sampleDataEntry.php">Sample Data Entry</a>
                     </li>
 					<li>
                         <a href="#">Perform a Comparison</a>
                     </li>
-                    <li class="dropdown">
+					<li>
 						<a href="" class="dropdown-toggle" data-toggle="dropdown">Databases<b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">Sous page 1</a>
-                                </li>
-								<li>
-                                    <a href="#">Sous Page 2</a>
-                                </li>
-                            </ul>
-                    </li>
-					<li class="dropdown">
-						<a role ="button" class="btn btn-primary" data-toggle="dropdown">Database Mangement<b class="caret"></b></a>
-                            <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
-                                <li>
-                                    <li class="dropdown2">
-										<a href="" class="dropdown-toggle" data-toggle="dropdown2">Genera<b class="caret"></b></a>
-											<ul class="dropdown-menu">
-												<li>
-													<a href="#">Add a new genus</a>
-												</li>
-												<li>
-													<a href="#">Find a genus</a>
-												</li>
-											</ul>
-									</li>
-                                </li>
-								<li>
-                                    <a href="#">Species Data</a>
-                                </li>
-								<li>
-                                    <a href="#">References</a>
-                                </li>
-								<li>
-									<li class="dropdown">
-										<a href="" class="dropdown-toggle" data-toggle="dropdown">Characters<b class="caret"></b></a>
-											<ul class="dropdown-menu">
-												<li>
-													<a href="#">Add a new characters</a>
-												</li>
-												<li>
-													<a href="#">List of characters</a>
-												</li>
-												<li>
-													<a href="#">Fill characters</a>
-												</li>
-											</ul>
-									</li>
-                                </li>
-								<li>
-                                    <a href="#">Users</a>
-                                </li>
-                            </ul>
+                        <ul class="dropdown-menu">
+                             <li><a href="#">Sous page 1</a></li>
+							 <li><a href="#">Sous Page 2</a></li>							 
+                         </ul>
+                    </li>					
+					<li>
+						<a href="#" class="dropdown-toggle"  data-toggle="dropdown">Database Mangement<span class="caret"></span></a>
+                        <ul class="dropdown-menu">   
+							<li>
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Genera <span class="caret"></span></a>  
+								<ul class="dropdown-menu">
+									<li><a href="#">Add a new genus</a></li>
+									<li><a href="#">Find a genus</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Species Data<span class="caret"></span></a>  
+								<ul class="dropdown-menu">
+									<li><a href="#">Add a new description</a></li>
+									<li><a href="#">Find a species</a></li>
+								</ul>
+							</li>
+							<li><a href="#">References</a></li>
+							<li>
+								<a href="" class="dropdown" data-toggle="dropdown">Characters<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Add a new characters</a></li>
+									<li><a href="#">List of characters</a></li>
+									<li><a href="#">Fill characters</a></li>
+								</ul>
+							</li>					
+							<li>
+								<a href="#">Users</a>
+							</li>
+                        </ul>
                     </li>
 					<li>
                         <a href="help.php">Help</a>
@@ -88,5 +82,13 @@
             </div>
             <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
+		<script>
+			$(document).ready(function(){
+			$('.dropdown-submenu a.test').on("click", function(e){
+				$(this).next('ul').toggle();
+				e.stopPropagation();
+				e.preventDefault();
+			});
+			});
+		</script>		
     </nav>
