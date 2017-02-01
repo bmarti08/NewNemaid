bc84d18579bdd71cc4b87dde9ad723ab243d34f3
MathJ <mathilde.jacquemart@gmail.com>
2017-02-01 10:50:22 +0100

    Creation de perform a comparison

---
+++ PerformComparison.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>NEMAID</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/portfolio-item.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <?php include("header.php"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-------------------------- Container --------------------------------->
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Comparison performance</h1>    
+					<h2 class="second-title"> Sample selection </h2>
+					<h3 class="third_tiltle"> Your samples </h3>
+                </div>
+            </div>
+			<br/>				
+			
+            <!-- /.row -->
+        <br/>
+		<div class="row_text">
+			<h2 class="second-title2">Comparison Parameters</h2>	
+			<h3 class="third_tiltle2">Formula version</h3>
+			<h3 class="third_tiltle3">Validity of species</h3>
+			<h3 class="third_tiltle4">Type of description</h3>
+		</div>
+		
+		
+        
+            <!-- ICI mettre ce qu'on veut -->
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <!-- ------------------------ -->
+        
+        <!-------------------------- /Container --------------------------------->
+
+        
+    <?php include("footer.php"); ?> 
+    </div>
+    <!-- /content -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
