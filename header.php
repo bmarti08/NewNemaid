c9cd3ff94bf0c08f323aee34c66803eec845a5e9
MathJ <mathilde.jacquemart@gmail.com>
2017-02-13 09:00:47 +0100

    Mise à jour - correction des erreurs apres validation connexion

--- header.php
+++ header.php
@@ -14,14 +14,8 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">                    
-					<li>
-                        <a href="SelectGenus.php">Genus selection</a>
-                    </li>
                     <li>
                         <a href="sampleDataEntry.php">Sample Data Entry</a>
-                    </li>
-					<li>
-                        <a href="PerformComparison.php">Comparison Performance</a>
                     </li>
 					<li>
 						<a href="" class="dropdown-toggle" data-toggle="dropdown">Databases views<b class="caret"></b></a>
